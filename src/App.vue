ed92da541a1718916dc785f530256a08f8547e28
Gabriel Souza <gabrielsouzacontato.99@gmail.com>
2024-09-22 22:38:00 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,104 @@
+<script setup>
+    import { ref } from 'vue';
+
+    import { ClassicEditor, Bold, Essentials, Italic, Mention, Paragraph, Undo, Plugin, ButtonView } from 'ckeditor5';
+    import { Ckeditor } from '@ckeditor/ckeditor5-vue';
+
+    import "ckeditor5/ckeditor5.css";
+    import "ckeditor5-premium-features/ckeditor5-premium-features.css";
+
+    class mentionPlugin extends Plugin {
+        init() {
+            const editor = this.editor;
+
+            editor.model.schema.extend('paragraph', {
+                allowAttributes: ['class']
+            });
+
+            editor.conversion.for('upcast').attributeToAttribute({
+                model: {
+                    name: 'paragraph',
+                    key: 'class'
+                },
+                view: {
+                    name: 'p',
+                    key: 'class'
+                }
+            });
+
+            editor.conversion.for('downcast').attributeToAttribute({
+                model: {
+                    name: 'paragraph',
+                    key: 'class'
+                },
+                view: {
+                    name: 'p',
+                    key: 'class'
+                }
+            });
+
+            editor.commands.get('enter').on('execute', (evt, data) => {
+                const selection = editor.model.document.selection;
+                const position = selection.getFirstPosition();
+                const parentElement = position.parent;
+
+                if (parentElement.is('element', 'paragraph') && parentElement.getAttribute('class') === 'quote') {
+                    evt.stop();
+
+                    editor.model.change(writer => {
+                        const paragraph = writer.createElement('paragraph');
+                        editor.model.insertContent(paragraph, editor.model.document.selection);
+                    });
+                }
+            });
+
+            editor.ui.componentFactory.add('mention', locale => {
+                const button = new ButtonView(locale);
+
+                button.set({
+                    label: 'Inserir Texto',
+                    withText: true,
+                });
+
+                button.on('execute', () => {
+                    editor.model.change(writer => {
+                        const blockquote = writer.createElement('paragraph', { class: 'quote' });
+                        const textNode = writer.createText('Este é um exemplo de citação.');
+
+                        writer.append(textNode, blockquote);
+
+                        editor.model.insertContent(blockquote, editor.model.document.selection);
+                    });
+
+                });
+
+                return button;
+            });
+        }
+    }
+
+    const editor = ClassicEditor;
+    const editorData = ref('<p>Hello from CKEditor 5 in Vue!</p>');
+
+    const editorConfig = {
+        plugins: [Bold, Essentials, Italic, Mention, Paragraph, Undo, mentionPlugin],
+        toolbar: ['undo', 'redo', 'bold', 'italic', 'mention'],
+        licenseKey: 'QkIzcDFtb0IzZkxtWHozeC9iT3JYZ1V6akpQcGkzaTQzSDFxWjViY3FQd2d0SWZwdlprZHQ1M1BVRzBobUE9PS1NakF5TkRFd01qST0=',
+    };
+</script>
+
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+    <div id="app">
+        <Ckeditor v-model="editorData" :editor="editor" :config="editorConfig" />
+    </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+    .quote {
+        border-left: 2px solid rgb(24, 131, 33);
+        color: #ccc;
+        font-style: italic;
+        padding-left: 15px;
+        margin: 10px 0;
+    }
 </style>
